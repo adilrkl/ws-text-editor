5697d6d0eed173c693848de113ce3210333dbe9c
adilrkl <adil464455@gmail.com>
2025-08-15 16:09:01 +0300

    little update for render deployment

--- src/main/java/com/example/wsTextEditor/config/SecurityConfig.java
+++ src/main/java/com/example/wsTextEditor/config/SecurityConfig.java
@@ -12,8 +12,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.core.userdetails.User;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
