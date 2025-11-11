335fcf1f4aa4772e259fbf0076eb6b08f321e416
Adil Arkalı <81088531+adilrkl@users.noreply.github.com>
2025-11-11 05:39:08 +0300

    Add test key to main application

--- src/main/java/com/example/wsTextEditor/WsTextEditorApplication.java
+++ src/main/java/com/example/wsTextEditor/WsTextEditorApplication.java
@@ -8,6 +8,6 @@ public class WsTextEditorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WsTextEditorApplication.class, args);
-
+String testkey= "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";
 	}
 }
